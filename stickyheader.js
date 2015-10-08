25e31a549d8d082d988dcc65147c6ffdadae7d25
Arturo Herrero <aehcreativeservices@gmail.com>
2015-10-08 10:17:23 -0400

    Updates to site navigation

--- stickyheader.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  var $header = $("header"),
-      $clone = $header.before($header.clone().addClass("clone"));
-
-  $(window).on("scroll", function() {
-    var fromTop = $("body").scrollTop();
-    $('body').toggleClass("down", (fromTop > 200));
-  });
-});
